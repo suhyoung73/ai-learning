eb6dd42bd0dd8ca97e12702c3ba775e73fedc1b4
suhyoung73 <suhyoung730@gmail.com>
2025-05-14 09:02:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with c2:
     st.markdown("#### 📂 지도학습")
     col1, col2, col3 = st.columns([1, 4, 4])
     with col2:
-        st.page_link("pages/예측.py", label="1-1) 예측", icon="📏")
+        st.page_link("pages/예측.py", label="1-1) **예측**", icon="📏")
     with col3:
         st.page_link("pages/분류.py", label="1-2) 분류", icon="📚")
 
@@ -17,7 +17,7 @@ with c4:
     st.markdown("#### 📂 비지도학습")
     col1, col2 = st.columns([1, 8])
     with col2:
-        st.page_link("pages/군집.py", label="2-1) 군집", icon="🧑‍💼")
+        st.page_link("pages/군집.py", label="2-1) 군집", icon="💰")
 c5, c6 = st.columns([1, 10])
 with c6:
     st.markdown("#### 📂 강화학습")
